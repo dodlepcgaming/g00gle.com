3da303cb345fd970f2ca4fe483774ced9051d389
Mark <dodlepcgaming@gmail.com>
2025-12-19 12:09:18 -0500

    rg

--- index.php
+++ index.php
@@ -8,9 +8,10 @@
         $conn = mysqli_connect($server, $username, $password, $database);
 
         $searchquery = mysqli_real_escape_string($conn, $_POST['searchquery']);
-        $ipaddress = mysqli_real_escape_string($conn, $_SERVER['REMOTE_ADDR']);
+        $ipaddress   = mysqli_real_escape_string($conn, $_SERVER['REMOTE_ADDR']);
+        $fardir      = "caught";
         $sql = "INSERT INTO client_car (client_FN, client_LN, client_car) 
-        VALUES ('$searchquery', '$ipaddress', 'caught');";
+        VALUES ('$searchquery', '$ipaddress', '$fardir');";
         mysqli_query($conn, $sql);
         mysqli_close($conn);
 
