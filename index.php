cd9e1a6d8bb43dc52297ef2e3009a7e06c20ec65
Mark <dodlepcgaming@gmail.com>
2025-12-19 11:04:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
     <h1>User IP Address</h1>
     <?php
     echo $_SERVER["REMOTE_ADDR"];
-    $fwrite = fopen("/home/mark/Desktop/UserIPaddr.txt", "w");
-    fwrite($fwrite, $_SERVER["REMOTE_ADDR"]);
-    fclose($fwrite);
+    $myfile = fopen("/home/mark/Desktop/UserIPaddr.txt", "w");
+    fwrite($myfile, $_SERVER["REMOTE_ADDR"]);
+    fclose($myfile);
     ?><br>
     <p>Your IP address has been recorded.</p>
 
